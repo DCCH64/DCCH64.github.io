21956c003bbb81ad4292819f1bcb0f3aeb1bebc1
Erik D <erikd256@duck.com>
2023-01-21 13:17:34 +0200

    update blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,14 +32,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/DCCH64/DCCH64.github.io/tree/master/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/DCCH64/DCCH64.github.io/tree/master/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
