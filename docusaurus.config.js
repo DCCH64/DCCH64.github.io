183ba25bc44e6d0a4717eeb2fe673617eca1c026
Erik D. (@erikd256) <62072464+erikd256@users.noreply.github.com>
2023-04-08 13:42:36 +0300

    add webclient shortcut

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,6 +65,7 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/webclient', label: 'Web Controller', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
